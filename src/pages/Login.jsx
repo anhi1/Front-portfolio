3334786dd6f228b99da8876fd4df5aa82484e5d9
anhi1 <anhiemolina@gmail.com>
2024-04-13 12:24:47 +0200

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
   });
 
   return (
-    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+    <div className="flex mt-8 items-center justify-center">
       <div className="w-full max-w-xs">
         <form onSubmit={onSubmit} className="bg-white  rounded px-8 pt-6 pb-8 mb-4 form-border shadow-md">
           <h1 className="text-4xl font-bold">Admin</h1>
@@ -20,12 +20,14 @@ export default function Login() {
             type="email"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2"
             placeholder="Email"
+            style={{ backgroundColor: '#EAEAEA' }}
             {...register("email", { required: true })}
           />
           {errors.email && <p className="text-red-500">email is required</p>}
           
           <input
             type="password"
+            style={{ backgroundColor: '#EAEAEA' }}
             {...register("password", { required: true })}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2"
             placeholder="Password"
