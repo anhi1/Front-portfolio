16c5fbce356b009fef5c4cf2cd98b6e82fd51cea
anhi1 <anhiemolina@gmail.com>
2024-04-25 12:10:54 +0200

    agreg vite config y en el main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { co2 } from '@tgwf/co2';
 
+
+// Crear una instancia de CO2
+const swd = new co2();
+// O puedes declarar explícitamente el modelo
+const declaredSwd = new co2({ model: "swd" });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-
+  },
+  resolve: {
+    alias: {
+      // Descomenta y ajusta según sea necesario
+      "CO2": "@tgwf/co2",
+    },
   }
 })
